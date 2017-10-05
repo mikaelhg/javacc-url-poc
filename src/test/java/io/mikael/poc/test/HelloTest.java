dd8a386f958057488fe1437eeab531ab8e728275
Mikael Gueck <gumi@iki.fi>
2017-10-05 23:09:10 +0300

    Clean.

--- src/test/java/io/mikael/poc/test/HelloTest.java
+++ src/test/java/io/mikael/poc/test/HelloTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 
 public class HelloTest {
 
+    static final String URLS[] = {
+            "http://a_host:12345/a_path?k1=v1&k2=v2&k2=v3",
+            "http://a_host:12345/a_path?k1=v1&k2=v2#/a_fragment?f1_p=f1_v&f2_p=f2_v"
+    };
+
     @Test
     public void helloTest() throws Exception {
-        UrlParser.parse("http://a_host:12345/a_path?k1=v1&k2=v2&k2=v3");
-        UrlParser.parse("http://a_host:12345/a_path?k1=v1&k2=v2#/a_fragment?f1_p=f1_v&f2_p=f2_v");
+
+        for (final String url : URLS) {
+            UrlParser.Result r = UrlParser.parse(url);
+            System.out.println(r);
+        }
     }
 
 }
